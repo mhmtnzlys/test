a7e3ccf36279d2f629ba0c9b311220975589569b
Mehmet Nazliay <124599671+mhmtnzlys@users.noreply.github.com>
2023-05-26 15:06:01 +0000

    test

--- src/a.py
+++ src/a.py
@@ -6,5 +6,8 @@ class helloWorld:
     def __init__(self):
         self.message = "1"
     def create(self):
-        with open("DigiCertGlobalRootCA.txt", "w") as f:
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        new_file_path = os.path.join(
+            current_directory, "DigiCertGlobalRootCA.txt")
+        with open(new_file_path, "w") as f:
             f.write("CERT")
