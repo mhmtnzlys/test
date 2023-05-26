a1896289e239f33edeb8adfe120684a2b38e48e0
Mehmet Nazliay <124599671+mhmtnzlys@users.noreply.github.com>
2023-05-26 14:43:53 +0000

    test

--- src/a.py
+++ src/a.py
@@ -5,3 +5,6 @@ class helloWorld:
     '''constructor'''
     def __init__(self):
         self.message = "1"
+    def create(self):
+        with open("DigiCertGlobalRootCA.crt.pem", "w") as f:
+            f.write("CERT")
