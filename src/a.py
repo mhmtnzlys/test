927d3b8a8d89a86ef0136dd3376d04e710c5d1f0
mhmtnzlys <mehmet.nazliay@shell.com>
2023-10-13 13:26:06 +0200

    test

--- src/a.py
+++
@@ -1,7 +0,0 @@
-import os
-
-'''A hello world class contains a Hello World! message'''
-class helloWorld:
-    '''constructor'''
-    def __init__(self):
-        self.message = "1"
--- y.py
+++
@@ -1,6 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-ACCESS_DATA_HOST = os.environ.get("MYSQL_ACCESS_DATA_USER")
-print(ACCESS_DATA_HOST)
